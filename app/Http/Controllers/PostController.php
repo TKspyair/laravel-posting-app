dce10a2b6b0c2fb0b622598dbfdad180bf5e2d5f
TKspayair <tkworking2003@gmail.com>
2025-03-30 10:39:04 +0900

    ok

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,12 +12,8 @@ class PostController extends Controller
   // 一覧ページ
   public function index()
   {
-    $posts = Auth::user()->posts()->orderBy('created_at', 'desc')->get();
-    $posts = Auth::user()->posts()->orderBy('updated_at','asc')->get();
-  
-
+    $posts = Auth::user()->posts()->orderBy('updated_at')->get();
     return view('posts.index', compact('posts'));
-  
   }
 
   // 詳細ページ
